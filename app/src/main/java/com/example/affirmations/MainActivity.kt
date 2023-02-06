7cf94916c101d3b73d2834b31684cb54d45d5e06
Cheruiyot <korirkipnisden@gmail.com>
2023-02-05 19:52:41 -0500

    updated recyclerview

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,16 +2,22 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
 import com.example.affirmations.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val myDataset = DataSource().loadAffirmations()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
+
+        val  recycler = binding.recyclerView
+        recycler.adapter = ItemAdapter(this, myDataset)
+        recycler.setHasFixedSize(true)
+   }
 }
--- app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations/adapter/ItemAdapter.kt
@@ -1,4 +1,33 @@
 package com.example.affirmations.adapter
 
-class ItemAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.databinding.ListItemBinding
+import com.example.affirmations.model.Affirmation
+
+class ItemAdapter(
+    private val context: Context,
+    private val dataset: List<Affirmation>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>(){
+
+    inner class ItemViewHolder(val binding: ListItemBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val binding = ListItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent,false)
+    return ItemViewHolder(binding)
+    }
+
+    override fun getItemCount() = dataset.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        with(holder){
+            with(dataset[position]){
+                binding.itemTitle.text = context.resources.getString(dataset[position].stringResourceId)
+            }
+        }
+    }
+
+
 }
